f7de23330081f22fc83b8fbdcc9851684567ea7a
aulyarvlln <aulyarvlln837@gmail.com>
2026-05-14 19:32:26 +0800

    vendor aku isi, event proses

--- src/models/Vendor.java
+++ src/models/Vendor.java
@@ -9,5 +9,24 @@ package models;
  * @author ACER
  */
 public class Vendor {
+    private int id;
+    private String name;
+    private String category;
+    private double minPrice;
+    private double maxPrice;
+    private String unitType;
+    private String contact;
+    private double rating;
+    private String notes;
     
+//    constructor kosong
+    public Vendor() {}
+    
+//    constructor tanpa id - untuk insert baru
+    public Vendor(String name, String category, double minPrice, double maxPrice, String unitType, String contact, double rating, String notes) {
+        this.name = name;
+        this.category = category;
+        this.minPrice = minPrice;
+        this.maxPrice = 
+    }
 }
